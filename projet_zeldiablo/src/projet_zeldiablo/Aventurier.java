dea745678a33f3ddeaedcd8e5ceff4c76db5ac83
Cornette Pierre <pierre.cornette8@etu.univ-lorraine.fr>
2019-05-31 11:49:46 +0200

    Aventurier.java -> constructeur. Entitee -> constructeur, setPos, setLabyrinthe

--- projet_zeldiablo/src/projet_zeldiablo/Aventurier.java
+++ projet_zeldiablo/src/projet_zeldiablo/Aventurier.java
@@ -1,5 +1,14 @@
 package projet_zeldiablo;
 
-public class Aventurier {
+public class Aventurier extends Entitee {
 
+	public Aventurier(int pv, int posX, int posY) {
+		super(pv, posX, posY);
+}
+
+	@Override
+	public boolean seDeplacer(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- projet_zeldiablo/src/projet_zeldiablo/Entitee.java
+++ projet_zeldiablo/src/projet_zeldiablo/Entitee.java
@@ -1,5 +1,40 @@
 package projet_zeldiablo;
 
 public abstract class Entitee {
+	private int pv;
+	private int posX;
+	private int posY;
+	private Labyrinthe lab;
 
+	public Entitee(int vie, int px, int py) {
+		if (vie < 0) {
+			this.pv = 0;
+		} else {
+			this.pv = vie;
+		}
+
+		if (px < 0) {
+			this.posX = 1;
+		} else {
+			this.posX = px;
+		}
+
+		if (py < 0) {
+			this.posY = 1;
+		} else {
+			this.posY = px;
+		}
+	}
+
+	public abstract boolean seDeplacer(int x, int y);
+
+	public void setPos(int posX) {
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	public void setLabyrinthe(Labyrinthe lb) {
+		this.lab = lb;
+	}
+	
 }
