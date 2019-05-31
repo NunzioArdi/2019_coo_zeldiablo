1204ce5b40c936d73d91a8dee3788648a0da0f27
NUNZIO_CONTE2 <nunzio.conte2@etu.univ-lorraine.fr>
2019-05-31 11:45:23 +0200

    ajout de la classe chemin

--- projet_zeldiablo/src/projet_zeldiablo/Chemin.java
+++ projet_zeldiablo/src/projet_zeldiablo/Chemin.java
@@ -1,5 +1,19 @@
 package projet_zeldiablo;
 
-public class Chemin {
+/** 
+ * Classe repésentant les chmemin du labyrinthe qui sont un type de Case.
+ * 
+ * @author CONTE Nunzio
+ */
+public class Chemin extends Case implements Traversable {
 
+	/** Constructeur de Case.
+	 * 
+	 * @param x abscisse.
+	 * @param y ordonnée.
+	 */
+	public Chemin(int x, int y) {
+		super(x,y);
+	}
+	
 }
