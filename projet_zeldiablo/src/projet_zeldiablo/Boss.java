26cd6b017290abfc30bd2e9e732a66bed802e4ee
Cornette Pierre <pierre.cornette8@etu.univ-lorraine.fr>
2019-06-06 15:55:08 +0200

    Ajout d'un sprite pour le boss

--- projet_zeldiablo/src/projet_zeldiablo/Boss.java
+++ projet_zeldiablo/src/projet_zeldiablo/Boss.java
@@ -3,6 +3,8 @@ package projet_zeldiablo;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import moteurJeu.sprite.Sprites;
+
 /**
  * Le Boss final du jeuej
  * 
@@ -17,11 +19,35 @@ public class Boss extends Monstre {
 	 */
 	public Boss(int vie) {
 		super(vie);
+		Sprites.chargerFeuille("Boss", "images\\Boss.png", 3, 4);
+
 	}
 
 	@Override
 	public void dessiner(Graphics g) {
-		g.setColor(Color.PINK);
-		g.fillOval(super.getX()*DessinJeuZeldiablo.TAILLE, super.getY()*DessinJeuZeldiablo.TAILLE, 42, 42);
+		g.setColor(Color.RED);
+		g.fillRect(super.getX() * DessinJeuZeldiablo.TAILLE + 5, super.getY() * DessinJeuZeldiablo.TAILLE - 20, 7 * 7,
+				10);
+		g.setColor(Color.GREEN);
+		g.fillRect(super.getX() * DessinJeuZeldiablo.TAILLE + 5, super.getY() * DessinJeuZeldiablo.TAILLE - 20,
+				this.getPV() * 7, 10);
+		
+		switch (super.direction) {
+		case 0:
+			Sprites.dessiner(g, "Boss_0_2", super.getX() * DessinJeuZeldiablo.TAILLE, super.getY() * DessinJeuZeldiablo.TAILLE);
+			break;
+		case 1:
+			Sprites.dessiner(g, "Boss_0_3", super.getX() * DessinJeuZeldiablo.TAILLE, super.getY() * DessinJeuZeldiablo.TAILLE);
+			break;
+		case 2:
+			Sprites.dessiner(g, "Boss_0_0", super.getX() * DessinJeuZeldiablo.TAILLE, super.getY() * DessinJeuZeldiablo.TAILLE);
+			break;
+		case 3:
+			Sprites.dessiner(g, "Boss_0_1", super.getX() * DessinJeuZeldiablo.TAILLE, super.getY() * DessinJeuZeldiablo.TAILLE);
+			break;
+			
+		default:
+			break;
+		}
 	}
 }
